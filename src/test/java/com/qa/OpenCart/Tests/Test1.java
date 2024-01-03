fcb5b8fd0712231bce9bc294db59b8efda4f65ef
savithat <savitha_tm@yahoo.com>
2024-01-02 19:21:44 -0800

    added 1st line in feature

--- src/test/java/com/qa/OpenCart/Tests/Test1.java
+++ src/test/java/com/qa/OpenCart/Tests/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("this is test1 in testJava folder");
 
-		
+		System.out.println("this is feature 1st line");
 
 	}
 
